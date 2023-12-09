68586d021696e9316349799a9f44180e9fa817e1
Tyler Critchlow <tylerdarincritchlow@gmail.com>
2023-12-09 14:02:16 -0700

    Added window renderer & legion

--- kaige-testing/src/main.rs
+++ kaige-testing/src/main.rs
@@ -19,23 +19,34 @@ struct Info {
 }
 
 fn main() {
+    // Create an instance of the App struct
+    let app = App::new();
+    app.run();
+    
+    // Create a new world
     let mut world = World::default();
 
     // Add entities to the world
     world.push((
         Position { x: 0.0, y: 0.0 },
         Velocity { x: 1.0, y: 1.0 },
-        Info { username: "tyler".to_string() }, 
+        Info {
+            username: "tyler".to_string(),
+        },
     ));
     world.push((
         Position { x: 5.0, y: 5.0 },
         Velocity { x: -1.0, y: -1.0 },
-        Info { username: "ashley".to_string() }, 
+        Info {
+            username: "ashley".to_string(),
+        },
     ));
     world.push((
-        Position { x: 10.0, y: 29.0},
-        Velocity { x: 0.0, y: 0.0},
-        Info { username: "brody".to_string() }, 
+        Position { x: 10.0, y: 29.0 },
+        Velocity { x: 0.0, y: 0.0 },
+        Info {
+            username: "brody".to_string(),
+        },
     ));
 
     // Search for all entities with values
@@ -43,19 +54,20 @@ fn main() {
 
     // Iterate over each entity
     for (info, position, velocity) in query.iter(&world) {
-        println!("Username: {:?}, Position: {:?}, Velocity: {:?}", info, position, velocity);
+        println!(
+            "Username: {:?}, Position: {:?}, Velocity: {:?}",
+            info, position, velocity
+        );
     }
 
     let result = simulate_error();
-    
+
     match result {
         Ok(_) => println!("Operation was successful!"),
-        Err(err) => {
-            match err {
-                Errors::TestError => println!("Encountered a kaiGE test error!"),
-                _ => println!("An unknown error occurred"),
-            }
-        }
+        Err(err) => match err {
+            Errors::TestError => println!("Encountered a kaiGE test error!"),
+            _ => println!("An unknown error occurred"),
+        },
     }
 }
 
@@ -63,4 +75,3 @@ fn main() {
 fn simulate_error() -> Result<(), Errors> {
     Err(Errors::TestError)
 }
-
