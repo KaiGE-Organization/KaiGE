3a945a38d3472f9ea6caaf9ed3114dfa18297a1b
Tyler Critchlow <tylerdarincritchlow@gmail.com>
2023-12-10 16:43:03 -0700

    added kaige_renderer crate //TODO

---
+++ crates/kaige_renderer/src/lib.rs
@@ -0,0 +1,25 @@
+pub struct Renderer {
+    //TODO
+}
+
+impl Renderer {
+    pub fn new() -> Self {
+        //TODO
+    }
+
+    pub fn render(&self) {
+        //TODO
+    }
+}
+
+impl Renderer {
+    pub fn new() -> Self {
+        Renderer {
+            //TODO
+        }
+    }
+
+    pub fn render(&self) {
+        //TODO
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod custom_errors;
 
 pub use kaige_ecs::*;
 pub use kaige_window::*;
+pub use kaige_renderer::*;
